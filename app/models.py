2e6535b49eaa2d1ed2c3a466954465fd05c3d174
plutusdev <devansh.patel@plutustec.com>
2022-06-16 19:16:01 +0530

    validation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.password_validation import validate_password
 from django.core.validators import RegexValidator
 from django.db import models
-from django.contrib.auth.models import AbstractUser,User
+from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
 from rest_framework.validators import UniqueValidator
@@ -12,9 +12,10 @@ CHOICES = (
 )
 
 Discount = (
-    ('flat','flat'),
-    ('percentage','percentage')
+    ('flat', 'flat'),
+    ('percentage', 'percentage')
 )
+
 
 # class Creation(AbstractUser):
 #     gender = models.CharField(max_length=40, choices=CHOICES)
@@ -30,30 +31,29 @@ class User(AbstractUser):
         return str(self.username)
 
 
-
-
 class Coupon(models.Model):
-    Coupon_REGEX = RegexValidator("[A-Z]+", 'only valid email is required')
+    Coupon_REGEX = RegexValidator("[A-Z]+", 'USE CAPITAL LETTER')
     coupon = models.CharField(max_length=6, validators=[Coupon_REGEX])
     create_date = models.DateField()
     expiry_date = models.DateField()
-    user = models.ForeignKey(User, related_name= "user" , on_delete =models.CASCADE)
+    user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
     gender = models.CharField(max_length=20, choices=CHOICES)
     discount = models.IntegerField()
-    discount_type = models.CharField(max_length=20 ,choices=Discount)
-    active = models.BooleanField()
+    discount_type = models.CharField(max_length=20, choices=Discount)
+    active = models.BooleanField(default=True)
     user_limit = models.IntegerField()
-    max_user =  models.IntegerField()
+    max_user = models.IntegerField(default=1)
+
 
     def __str__(self):
         return self.coupon
 
+
 class Product(models.Model):
     coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     total = models.IntegerField()
-
 
 
--- app/serializers.py
+++ app/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import *
-
+from django.utils import timezone
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -20,6 +20,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             'first_name': {'required': True},
             'last_name': {'required': True}
         }
+
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -49,9 +50,8 @@ class Productserializers(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['coupon','user','price','name','total']
-        read_only_fields =["total"]
-
+        fields = ['coupon', 'user', 'price', 'name', 'total']
+        read_only_fields = ["total"]
 
     def validate(self, attrs):
         coupon = attrs.pop('coupon', None)
@@ -61,7 +61,7 @@ class Productserializers(serializers.ModelSerializer):
 
         coupon = Coupon.objects.get(coupon=coupon)
         user = self.context.get('request').user
-        birthdate = User.objects.filter(birthdate= user.birthdate).first()
+        birthdate = User.objects.filter(birthdate=user.birthdate).first()
 
         if coupon.discount_type == "flat":
             total = price - coupon.discount
@@ -69,19 +69,40 @@ class Productserializers(serializers.ModelSerializer):
             birthdate = birthdate.birthdate
             validdate = datetime.datetime.now().strftime("%m-%d")
 
-            if birthdate.stfrtime("%m-%d") == validdate:
+            if birthdate and birthdate.strftime("%m-%d") == validdate:
                 discount = price * (coupon.discount / 100)
                 discount_price = price - discount
-                total = discount_price-(discount_price*0.1)
+                total = discount_price - (discount_price * 0.1)
             else:
                 discount = price * (coupon.discount / 100)
                 total = price - discount
+        coupon_use_limit = coupon.max_user
+        coupon_usee_limit = coupon.user_limit
+
+        if user.is_authenticated:
+
+            user_count = len(Product.objects.filter(user=user, coupon=coupon))
+            coupon_count = len(Product.objects.filter(coupon=coupon))
+
+            if coupon_count > coupon_usee_limit:
+                raise serializers.ValidationError("Coupon limit is over")
+
+            if user_count > coupon_use_limit:
+                raise serializers.ValidationError("Per user limit is over")
+
+            coupon.max_limit = coupon.max_user - 1
+            coupon.save()
+        else:
+            raise serializers.ValidationError("choose different coupon or coupon limit is over")
 
         attrs['coupon'] = coupon
+        attrs['user_id'] = user.id
         attrs['total'] = total
         return attrs
+
 
 class Couponserializer(serializers.ModelSerializer):
     class Meta:
         model = Coupon
-        fields = "__all__"
+        fields = ['coupon','create_date','expiry_date','user','gender','discount','discount_type','user_limit','max_user']
+        raed_only_field = ['is_active']
